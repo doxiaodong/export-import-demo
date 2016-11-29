fa5171834f96aac5d1c30b35c2753d764a117ac3
darlin <duxiaodong@darlin.local>
2016-11-29 17:11:11 +0800

    add accept and max size

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { exportCSV } from 'export.js/csv'
 import { Data } from './data'
 import { toData } from './to-data'
 import { dataToTable } from './data-to-table'
+
 const downloadButton = document.getElementById('download')
 const uploadInput = document.getElementById('upload')
 const content = document.getElementById('content')
+const maxSize = 5 * 1024
 
 content.innerHTML = dataToTable(Data)
 
@@ -19,12 +21,21 @@ fileReader.addEventListener('load', (event) => {
   content.innerHTML = dataToTable(toData(text))
 })
 
+fileReader.addEventListener('error', (event) => {
+  console.error('File read error')
+  alert('File read error')
+})
+
 uploadInput.addEventListener('change', (event) => {
   const files = event.target['files']
   if (files.length === 0) {
     return
   }
   const file = files[0]
-
+  if (file.size > maxSize) {
+    console.error('Max file size is 5kb')
+    alert('Max file size is 5kb')
+    return
+  }
   fileReader.readAsText(file)
 })
